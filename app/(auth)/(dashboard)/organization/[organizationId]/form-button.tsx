abbb942da3b602d965b50650f1efc6d1ce00b93f
selimcan sağdıç <selimcanscs1907@gmail.com>
2024-07-23 21:16:27 +0300

    create use action

--- app/(auth)/(dashboard)/organization/[organizationId]/form-button.tsx
+++ app/(auth)/(dashboard)/organization/[organizationId]/form-button.tsx
@@ -4,7 +4,7 @@ import { useFormStatus } from "react-dom";
 export const FormButton = () => {
   const { pending } = useFormStatus();
   return (
-  <Button disabled={pending} type="submit">
+  <Button disabled={pending} type="submit" className="flex flex-col items-center w-64 mt-4">
     Submit
     </Button>
     );
