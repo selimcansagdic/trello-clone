275c1fd8663404dd2c277525b9b8c103cc6ac8a5
selimcan sağdıç <selimcanscs1907@gmail.com>
2024-08-09 03:35:21 +0300

    updated

--- app/(auth)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ app/(auth)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -1,14 +1,21 @@
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Info } from "../_components/info";
 import { Separator } from "@/components/ui/separator";
 import { ActivityList } from "./_components/activity-list";
 import { checkSubscription } from "@/lib/subscription";
 
-interface ActivityPageProps {
-  isPro: boolean;
-}
+const ActivityPage = () => {
+  const [isPro, setIsPro] = useState(false);
 
-const ActivityPage = ({ isPro }: ActivityPageProps) => {
+  useEffect(() => {
+    const fetchSubscription = async () => {
+      const result = await checkSubscription();
+      setIsPro(result);
+    };
+
+    fetchSubscription();
+  }, []);
+
   return (
     <div className="w-full">
       <Info isPro={isPro} />
@@ -19,14 +26,5 @@ const ActivityPage = ({ isPro }: ActivityPageProps) => {
     </div>
   );
 };
-
-export async function getServerSideProps() {
-  const isPro = await checkSubscription();
-  return {
-    props: {
-      isPro,
-    },
-  };
-}
 
 export default ActivityPage;
